775d48b575aae0e42ad5e8a1143436b1c0875ff9
dietban19 <dieter.banaag@ucalgary.ca>
2024-02-24 12:05:46 -0700

    fix

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import './Dashboard.scss';
 import { useUserContext } from '../../context/userContext';
 import AssignmentPopup from '../../components/DashboardComponents/AssignmentPopup';
 import DbNavbar from '../../components/DashboardComponents/dbNavbar/dbNavbar';
-import DbSideBar from '../../components/DashboardComponents/dbSideBar/dbSideBar';
+import DbSideBar from '../../components/DashboardComponents/dbSideBar/dbSidebar';
 import UserCard from '../../components/DashboardComponents/DashboardCards/UserCard';
 import TodaysTasks from '../../components/DashboardComponents/DashboardCards/TodaysTasks';
 import DashboardCalendar from '../../components/DashboardComponents/DashboardCards/Calendar/DashboardCalendar';
@@ -18,28 +18,28 @@ const Dashboard = () => {
           <DbNavbar />
         </div>
         <div className="dashboard-middle">
-          <div className="dashboard-sidebar db-todo-container dark:bg-dark-800 min-h-16 w-1/2  rounded-lg bg-white p-4 transition duration-200">
+          <div className="dashboard-sidebar db-todo-container min-h-16 w-1/2 rounded-lg  bg-white p-4 transition duration-200 dark:bg-dark-800">
             <DbSideBar />
           </div>
           <div className="dashboard-main-container">
             <div className="db-main-top flexCol gapOne">
-              <div className="dashboard-user db-todo-container dark:bg-dark-800 min-h-16 w-1/2  rounded-lg bg-white p-4 transition duration-200">
+              <div className="dashboard-user db-todo-container min-h-16 w-1/2 rounded-lg  bg-white p-4 transition duration-200 dark:bg-dark-800">
                 <UserCard />
               </div>
-              <div className="db-todo-container dark:bg-dark-800 min-h-16 w-1/2  rounded-lg bg-white p-4 transition duration-200">
+              <div className="db-todo-container min-h-16 w-1/2 rounded-lg  bg-white p-4 transition duration-200 dark:bg-dark-800">
                 <TodaysTasks />
               </div>
             </div>
             <div className="mb-4 flex gap-4 ">
-              <div className="db-todo-container dark:bg-dark-800 min-h-16 w-1/2  rounded-lg bg-white p-4 transition duration-200">
+              <div className="db-todo-container min-h-16 w-1/2 rounded-lg  bg-white p-4 transition duration-200 dark:bg-dark-800">
                 <ToDo />
               </div>{' '}
-              <div className="dashboard-pomodoro db-todo-container dark:bg-dark-800 min-h-16 w-1/2  w-1/2 rounded-lg bg-white p-4 transition duration-200">
+              <div className="dashboard-pomodoro db-todo-container min-h-16 w-1/2 w-1/2  rounded-lg bg-white p-4 transition duration-200 dark:bg-dark-800">
                 <DashboardPomodoro />
               </div>
             </div>
 
-            <div className="dashboard-calendar db-todo-container dark:bg-dark-800 min-h-16 w-1/2  rounded-lg bg-white p-4 transition duration-200">
+            <div className="dashboard-calendar db-todo-container min-h-16 w-1/2 rounded-lg  bg-white p-4 transition duration-200 dark:bg-dark-800">
               <CalendarComponent />
             </div>
 
